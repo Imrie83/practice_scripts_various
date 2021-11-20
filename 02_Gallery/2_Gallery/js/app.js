200224b56aaa64da0f9e2dc1d7e5ab7f55c19dc5
Imrie83 <czolgista83@gmail.com>
2021-11-20 15:37:35 +0100

    a simple gallery with a javascript

--- 02_Gallery/2_Gallery/js/app.js
+++ 02_Gallery/2_Gallery/js/app.js
@@ -1,6 +1,32 @@
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
+const imgList = []
+document.querySelectorAll('li').forEach(function(li){
+    imgList.push(li);
+});
+const body = document.querySelector('body');
+
+imgList.forEach(function(img){
+    img.addEventListener('click', function(){
+        const imgDiv = document.createElement('div')
+        imgDiv.className = 'fullScreen';
+        body.appendChild(imgDiv);
+
+        const imgSource = this.querySelector('img').getAttribute('src');
+        const image = document.createElement('img');
+        image.setAttribute('src', imgSource);
+        imgDiv.appendChild(image);
+
+        const btn = document.createElement('button');
+        btn.className = 'close';
+        btn.innerText = 'Close';
+        imgDiv.appendChild(btn);
+
+        document.querySelector('.fullScreen').addEventListener('click', function(){
+            this.remove();
+        });
+
+        document.querySelector('.close').addEventListener('click', function(){
+            document.querySelector('.fullScreen').remove();
+        });
+    });
+});
+
