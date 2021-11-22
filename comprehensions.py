c6003a0a20922c4cd472754af6355a4a1019ef0b
Imrie83 <czolgista83@gmail.com>
2021-11-22 20:45:55 +0100

    comment added

--- comprehensions.py
+++ comprehensions.py
@@ -1,3 +1,5 @@
+# comprehensions are single line alternatives to creating a list, dictionary or generator
+
 my_list = [2 ** i for i in range(20)]
 my_dict = {f'position - {i + 1}:': 2 ** i for i in range(20)}
 
