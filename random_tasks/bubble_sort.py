3be3e6b5c58756b68548448b453fc913f8a4c97f
Imrie83 <czolgista83@gmail.com>
2021-11-23 12:22:45 +0100

    bubble sort example

---
+++ random_tasks/bubble_sort.py
@@ -0,0 +1,13 @@
+from random import randint
+
+my_list = [randint(0, 100) for i in range(10)]
+print(my_list)
+
+def bubble_sort(list):
+    for i in range(len(list)):
+        for j in range(len(list) - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
+    return list
+
+print(bubble_sort(my_list))
