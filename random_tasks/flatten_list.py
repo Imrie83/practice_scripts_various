4170a625f32201bfb2c31dab0915e5b8ce7a7d3f
Imrie83 <czolgista83@gmail.com>
2021-11-23 13:06:11 +0100

    flatten list recursive with a generator

---
+++ random_tasks/flatten_list.py
@@ -0,0 +1,12 @@
+lista = [1, 2, [3, 4, 5], [6, [7, 8, 9], 10], 11, 12]
+print(lista)
+
+def flatten(my_list):
+    for nested in my_list:
+        if isinstance(nested, list):
+            yield from flatten(nested)
+        else:
+            yield nested
+
+lista = list(flatten(lista))
+print(lista)
