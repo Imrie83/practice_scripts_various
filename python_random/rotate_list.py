11508d93c613f979763d53fcc640ce9bdd70ba80
Imrie83 <czolgista83@gmail.com>
2021-11-24 17:14:24 +0100

    list rotation using two different ways

--- python_random/rotate_list.py
+++ python_random/rotate_list.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
+
+# faster version
 def rotate(my_list, num_rotations):
+    start = datetime.now()
     if num_rotations == len(my_list):
         return my_list
 
@@ -8,7 +13,25 @@ def rotate(my_list, num_rotations):
     my_slice = my_list[0:-num_rotations]
     my_list = my_list[-num_rotations:]
     my_list.extend(my_slice)
+    print(datetime.now() - start)
     return my_list
+
+
+# slower version
+# def rotate(my_list, num_rotations):
+#     start = datetime.now()
+#     if num_rotations == len(my_list):
+#         return my_list
+#
+#     if num_rotations > len(my_list):
+#         num_rotations = num_rotations - len(my_list)
+#
+#     for i in range(len(my_list[:-num_rotations])):
+#         my_slice = my_list.pop(0)
+#         my_list.append(my_slice)
+#
+#     print(datetime.now() - start)
+#     return my_list
 
 
 
