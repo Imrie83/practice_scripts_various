303adc3302cdaf3ca77589db05548c6d86cc6964
Imrie83 <czolgista83@gmail.com>
2021-11-24 16:45:23 +0100

    rotate list with slices

---
+++ python_random/rotate_list.py
@@ -0,0 +1,25 @@
+def rotate(my_list, num_rotations):
+    if num_rotations == len(my_list):
+        return my_list
+
+    if num_rotations > len(my_list):
+        num_rotations = num_rotations - len(my_list)
+
+    my_slice = my_list[0:-num_rotations]
+    my_list = my_list[-num_rotations:]
+    my_list.extend(my_slice)
+    return my_list
+
+
+
+
+
+
+#### TESTS SHOULD ALL BE TRUE ####
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1), ['f', 'a', 'b', 'c', 'd', 'e'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 1) == ['f', 'a', 'b', 'c', 'd', 'e']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2), ['e', 'f', 'a', 'b', 'c', 'd'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 2) == ['e', 'f', 'a', 'b', 'c', 'd']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3), ['d', 'e', 'f', 'a', 'b', 'c'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 3) == ['d', 'e', 'f', 'a', 'b', 'c']))
+
+print("{0}\n should equal \n{1}\n {2}\n".format(rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4), ['c', 'd', 'e', 'f', 'a', 'b'], rotate(['a', 'b', 'c', 'd', 'e', 'f'], 4) == ['c', 'd', 'e', 'f', 'a', 'b']))
